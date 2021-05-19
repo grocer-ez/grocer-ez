f66885a0b7bb00e9c1a425cb4880094b2777e5fc
bstanfield1992 <bstanfield1992@gmail.com>
2021-05-18 17:17:26 -0700

    changed first and last name to username sign up working

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -12,8 +12,7 @@ function Signup(props) {
     event.preventDefault();
     const mutationResponse = await addUser({
       variables: {
-        email: formState.email, password: formState.password,
-        firstName: formState.firstName, lastName: formState.lastName
+        email: formState.email, password: formState.password, username: formState.username
       }
     });
     const token = mutationResponse.data.addUser.token;
@@ -37,22 +36,12 @@ function Signup(props) {
       <h2>Signup</h2>
       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
+          <label htmlFor="username">Username:</label>
           <input
             placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
+            name="username"
+            type="username"
+            id="username"
             onChange={handleChange}
           />
         </div>
