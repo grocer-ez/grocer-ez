ea0a80dc4606a45abc6ac5247872fa0a800a369a
coricathemer <coricathemer@gmail.com>
2021-05-20 00:14:04 -0700

    updated utils/queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,14 +1,11 @@
 import gql from 'graphql-tag';
 
-export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
+export const QUERY_LISTS = gql`
+  query getList($category: ID) {
+    lists(category: $category) {
       _id
       name
-      description
-      price
       quantity
-      image
       category {
         _id
       }
@@ -16,17 +13,9 @@ export const QUERY_PRODUCTS = gql`
   }
 `;
 
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
+export const QUERY_ALL_LISTS = gql`
   {
-    products {
+    lists {
       _id
       name
       description
@@ -39,35 +28,14 @@ export const QUERY_ALL_PRODUCTS = gql`
   }
 `;
 
-export const QUERY_CATEGORIES = gql`
+export const QUERY_STORES = gql`
 {
-  categories {
+  stores {
     _id
     name
   }
 }
-`;
-
-// export const QUERY_USER = gql`
-// {
-//   user {
-//     firstName
-//     lastName
-//     orders {
-//       _id
-//       purchaseDate
-//       products {
-//         _id
-//         name
-//         description
-//         price
-//         quantity
-//         image
-//       }
-//     }
-//   }
-// }
-// `;
+`;    
 
 export const QUERY_USER = gql `
   {
