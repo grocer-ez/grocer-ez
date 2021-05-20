10a9443a7f25c5582fd5fdb797af0c7ad00e323c
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-05-20 16:48:57 -0700

    added structure to page

--- client/src/components/AllStores/index.js
+++ client/src/components/AllStores/index.js
@@ -9,8 +9,28 @@ const Home = () => {
     {id: "something2", name: "Frys", createdAt:"someDate", lists: ["computer parts", "comic books"]}
   ]
   return (
+    
     <div className="container">
-      <h1>test</h1>
+      {
+        listArray.map((element)=>{
+          return <>
+<div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+      <div class="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  </div>
+  </>
+        })
+      };
       {
         listArray.map((element)=>{
           return <>
@@ -22,9 +42,11 @@ const Home = () => {
                 <a href="/thislist/0" className="btn btn-primary">Go somewhere</a>
               </div>
             </div>
-          </>
+
+            </>
+
         })
-      }
+}
     </div>
   );
 };
