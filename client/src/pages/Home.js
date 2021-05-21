92475225c197cd8b85d4d27047bc1154b27d8b9f
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-05-21 14:09:07 -0700

    commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,11 +3,11 @@ import StoreList from '../components/StoreList';
 
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_STORES } from '../utils/queries';
+import { QUERY_STORES, QUERY_ME } from '../utils/queries';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_STORES);
-  // const { data: userData } = useQuery(QUERY_ME);
+  const { data: userData } = useQuery(QUERY_ME);
   const stores = data?.stores || [];
 
   const loggedIn = Auth.loggedIn();
