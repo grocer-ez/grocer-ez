bbcb85b98ae7da6d244e68ca1a3b93bbcf3d99fe
coricathemer <coricathemer@gmail.com>
2021-05-24 15:36:10 -0700

    Working single list page

--- client/src/pages/SingleList.js
+++ client/src/pages/SingleList.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import SingleList from '../components/SingleList';
-import Auth from '../utils/auth';
+// import SingleList from '../components/SingleList';
+// import Auth from '../utils/auth';
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/react-hooks';
 // import { QUERY_STORE } from '../utils/queries';
@@ -19,20 +19,35 @@ const List = props => {
   const store = data?.store || {};
   console.log("Store", store);
 
-  const loggedIn = Auth.loggedIn();
+  // const loggedIn = Auth.loggedIn();
   console.log("Name", data);
 
   return (
     <div>
-          {loggedIn && data ? (
-             <div className="card mb-3">
-                 <SingleList store={storeId} />
-               <h1> Display Store Name and list here </h1>
-             <div className="card-body">
-     
-             </div>
-           </div>
-          ) : null} 
+      <div className="card mb-3">
+    <p className="card-header">
+      <span style={{ fontWeight: 700 }} className="text-light">
+        {store.name}
+      </span>{' '}
+      {/* thought on {thought.createdAt} */}
+    </p>
+    <div className="card-body">
+      {store.list ?
+        store.list.map((element, index) =>{
+          return index % 2 ?
+          <div style={{ backgroundColor: "grey", color: "black"}}>
+            <h3>{element.item}</h3>
+            <p>Quantity: {element.quantity}</p>
+            </div> :
+            <div style={{ backgroundColor: "white", color: "black"}}>
+            <h3>{element.item}</h3>
+            <p>Quantity: {element.quantity}</p>
+          </div>
+        })
+        : <p>No List found</p>
+      }
+    </div>
+  </div>
     </div>
   );
 };
