b4edb2eba6c145876f972a4fcd5804ebe7e5671e
a7063p <andrewpennucci@yahoo.com>
2021-05-24 23:08:13 -0700

    update

---
+++ client/src/components/AddStore/index.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import { Modal } from "react-bootstrap";
+// import axios from 'axios';
+
+const AddStore = (props) => {
+  const [modalDisplay, setModalDisplay] = useState(false);
+
+
+  const inputSaveHandler = ()=>{
+ 
+    //just need correct url and correct query in the post request
+    
+  //https://github.com/axios/axios
+  //   axios.post(
+  //     'http://localhost:3001/graphql',
+  //     {
+  //       me:{
+  //       stores:{
+  //         name:"test"
+  //       }}
+  //     }
+  //   ) 
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+  // };
+  // const onClose = ()=>{
+  //   props.setModalDisplay(false);
+  }
+
+  return (
+   <div>
+    <h1>{props.userData}</h1>
+    <Modal show={true}>
+    <Modal.Header>{props.data.me.stores}</Modal.Header>
+    <Modal.Body>
+      <input type='text' id='storeName'></input>
+    </Modal.Body>
+    <Modal.Footer>
+     {/* <button onClick={onClose}>Close</button> */}
+    <button onClick={inputSaveHandler}>Save</button> 
+    </Modal.Footer>
+  </Modal>
+  </div>
+)
+};  
+
+export default AddStore;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,16 @@
 import gql from 'graphql-tag';
 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -11,43 +22,55 @@ export const LOGIN = gql`
   }
 `;
 
-export const ADD_LIST = gql`
-mutation addList($storeId: ID!, $item: String!, $quantity: Int!) {
-  addList(storeId: $storeId, item: $item, quantity: $quantity) {
-    _id
-    name
-    username
-    lists {
-      _id
-      item
-      quantity
-      username
-    }
-    
-  }
-}
-`;
+
 export const ADD_STORE = gql`
-mutation addStore($name: String!) {
-  addStore(name: $name) {
+mutation addStore($_id: ID!, $name: String!) {
+  addStore(_id: $_id, name: $name) {
     _id
     name
     createdAt
-    username    
+    username
+  }
+}
+`;
+
+export const UPDATE_STORE = gql `
+mutation updateStore($_id: ID!, $name: String!) {
+  updateStore(_id: $_id, name: $name) {
+    _id
+    name
   }
 }
 `;
 
 
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-      }
+
+export const ADD_LIST = gql`
+mutation addList($_id: ID!, $item: String!, $quantity: Int!) {
+  addList(_id: $_id, item: $item, quantity: $quantity) {
+    _id
+    name
+    list {
+      _id
+      item
+      quantity
     }
   }
+}
+`;
+
+export const REMOVE_ITEM = gql`
+mutation removeItem($storeId: ID!, $itemId: ID!) {
+  removeItem(storeId:$storeId, itemId:$itemId) {
+    _id
+    name
+    list {
+      _id
+      item
+      quantity
+    }
+  }
+}
 `;
 
 export const ADD_ITEM = gql`
@@ -61,14 +84,19 @@ export const ADD_ITEM = gql`
   }
 `;
 
-export const REMOVE_ITEM = gql`
-  mutation removeItem($id: ID!) {
-    removeItem(id: $id) {
+
+
+export const CLEAR_LIST = gql`
+mutation clearList($storeId: ID!) {
+  clearList(storeId: $storeId) {
+    _id
+    name
+    list {
       _id
-      username
-      item {
-        _id
-      }
+      item
+      quantity
     }
   }
+
+}
 `;
